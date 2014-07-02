0a2fcd38e1351f507ef64d89f61b8d7c25313d14
Michael Mastroianni <mmastroianni@placester.com>
2014-07-02 15:57:10 -0400

    updated instructions to make the test doable in the office

--- src/main/com/placester/test/ThreadSafePriorityQueue.java
+++ src/main/com/placester/test/ThreadSafePriorityQueue.java
@@ -1,16 +1,21 @@
 package com.placester.test;
 // NOTE: we are aware that there is a PriorityQueue in
-// java.util. Please do not use any containers from
+// java.util. PLease do not use this. 
+// If you are doing this test at home, please do not use any containers from
 // java.util in your solution, as this is a test of data
-// structure knowledge, rather than a test of java library knowledge
+// structure knowledge, rather than a test of java library knowledge.
+// If you are doing it in the office, please ask the person testing you if you are going to
+// use any built in collections other than arrays.
+
 /*
- * The task is as follows: implement this class as you see fit (modulo
- * not using any collections classes from java.util or
- * java,util.collections, and get the unit test in
+ * The task is as follows: implement this class as you see fit, and get the unit test in
  * src/test/com/placester/test/PriorityQueueTest to pass. This class
  * must allow dynamic resizing as elements are added. What the
  * strategy is to do this is entirely up to you modulo the previously
- * stated constraint.
+ * stated constraints.
+ * 
+ * Feel free to use anything from Java.util.Arrays (e.g., you don't need to implement
+ * your own sort if you don't want to).
  */
 public class ThreadSafePriorityQueue<X> implements SimpleQueue<Priority<X>>
 {
